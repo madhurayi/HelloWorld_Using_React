44eab5ffb09f3722fa89031ab9c6b8d913559af3
madhu <rayi.madhupriya@gmail.com>
2024-06-27 21:25:14 +0530

    Added h1 tag using jsx

--- app.js
+++ app.js
@@ -1,45 +1,20 @@
-import React from "react"
-import ReactDOM from "react-dom/client"
-const parent=React.createElement(
-    "div",
-    {id: "parent"},
-    React.createElement(
-        "div",
-        {id:"child"},
-        [ React.createElement(
-            "h1",
-            {id:"h1"},
-            "I am h2 tag"
-        ),
-        React.createElement(
-            "h2",
-            {id:"h2"},
-            "I am madhu"
-        ),
-        ,
-        React.createElement(
-            "h3",
-            {id:"h2"},
-            "I am priya"
-        )]
-    )
-)
+// import React from "react";
+// import ReactDOM from "react-dom/client";
 
-console.log(parent);
+// const hed = <h1>namastereact </h1>;
+// // const jsxheading =<h1> Namaste React Using JSX </h1>;
 
-const root= ReactDOM.createRoot(document.getElementById("root"));  
+// const root= ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(parent);  //render will take javascript object and convert it into dom that is understood by browser.
+// root.render(hed);  //render will take javascript object and convert it into dom that is understood by browser.
 
 
-// const heading=React.createElement(
-//     "h1",   //this is element
-//     {id:"heading",xyz:"abc"}, //we can set arributes in key values pairs
-//     "Hello World" //This is the content which we want inside element
-// );
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-// console.log(heading); //It wont print h1 tag, it is a React element or javascript object
+const hed = <h1>namastereact</h1>; // Assuming you want an h1 element
+console.log(hed);
 
-// const root= ReactDOM.createRoot(document.getElementById("root"));  
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
-// root.render(heading);  //render will take javascript object and convert it into dom that is understood by browser.
+root.render(hed); // Render the h1 element
