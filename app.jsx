e6efa345ef764682a50e732986ec5352abe48335
madhu <rayi.madhupriya@gmail.com>
2024-06-27 23:02:10 +0530

    Added component composition

--- app.jsx
+++ app.jsx
@@ -2,14 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 //React Element
-const hed = <h1 className="head">namastereact</h1>; // Assuming you want an h1 element
-console.log(hed);
+const TitleElement =()=> {
+    return (
+        <h1 className="head" tabIndex={1}>Namasthe React Using JSX</h1>
+    )
+}; // Assuming you want an h1 element
+console.log(TitleElement);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 const HeadingElement= ()=>(
-    <div id="container">
-        <h1 className="heading">Namasthe React Functional COmponent</h1>
+    <div id="container"> 
+        <TitleElement/>
+        <h1 className="heading">Namasthe React Functional Component</h1>
     </div>
     
 
