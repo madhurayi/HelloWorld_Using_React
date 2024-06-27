a9d69d124becc54d353709d953a2086d22b04e9d
madhu <rayi.madhupriya@gmail.com>
2024-06-27 23:50:31 +0530

    Added nested components

--- app.jsx
+++ app.jsx
@@ -2,17 +2,22 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 //React Element
-const TitleElement =()=> {
-    return (
-        <h1 className="head" tabIndex={1}>Namasthe React Using JSX</h1>
-    )
-}; // Assuming you want an h1 element
+const elem= <span>Namasthe React Component</span>
+const TitleElement =()=> (
+        <div>
+            {elem}
+            <h1 className="head" tabIndex={1}>Namasthe React Using JSX</h1>
+        </div>     
+        
+    ); 
 console.log(TitleElement);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 const HeadingElement= ()=>(
     <div id="container"> 
+        {TitleElement()}
+        <TitleElement></TitleElement>
         <TitleElement/>
         <h1 className="heading">Namasthe React Functional Component</h1>
     </div>
